8deacb73660e0ff01f5eb68165dd79c622cd7b69
tletutour <tom.le-tutour@insa-lyon.fr>
2019-12-06 21:28:32 +0200

    implementation of cross val successful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import csv
 import os
-from utility import get_good_batches
+from utility import get_good_batches,get_good_folds,split_in_batches
 from Networks import Hidden1 
 import torch.nn.functional as func
 import torch
@@ -29,59 +29,73 @@ for n in nets:
         for b in batch_sizes:
                 for l in losses:
                     for o in optims:
+                        
                         print("training",t,"batch size",b)
-                        #We load the data in batches
-                        data_list,class_list, test_set,test_class=get_good_batches(path,b,t)
-                        #Let's train the network
-                        thisnet=n
-                        loss_f=l
-                        o=optim.SGD(n.parameters(), lr=0.1)
-                        #nb of epochs
-                        e=0
-                        #If loss doesn't move anymore, we stop training
-                        old_loss=10000
-                        convergence=False
-                        while(e<max_epochs and convergence==False):
-                            e+=1
-                            for i in range(len(data_list)):
-                                batch=data_list[i]
-                                classes=class_list[i]
-                                outs=torch.zeros(b,10)
-                                for k in range(b):
-                                    outs[k,:]=n(batch[k,0,:,:])
-                                o.zero_grad() 
-        
-                                loss = criterion(outs,classes)
-                                loss.backward()
-                                o.step()
-                                
-                                if abs(loss.item())<0.00001:
-                                    print("loss is very small")
-                                    convergence=True
-                                    break
-                                if abs(loss.item()-old_loss)<0.0001:
-                                    print("loss does not move")
-                                    convergence=True
-                                    break
-                                old_loss=loss.item()
-                            if convergence:
-                                print("convergence condition reached after",e,"epochs")
-                                break
-                        if(convergence==False):
-                            print("training done, no convergence")
-                        #We will now validate the network
-                        errors=0
-                        for k in range(1000-t):
-                            output=n(test_set[k,0,:,:])
-                            predicted_class=output[0,:].max(0)[1]
-                            if predicted_class==test_class[k]:
+                        #We load the data in folds representing classes equally
+                        K=10
+                        target_fold, class_fold,image_fold=get_good_folds(path,K)
+                        for k in range(K):
+                            print("validating on fold ",k+1,"training on all the others")
+                            #We reinstantiate the network for each of the tested
+                            #folds otherwise the networks trains on data it already saw
+                            thisnet=n
+                            loss_f=l
+                            o=optim.SGD(n.parameters(), lr=0.1)
+                            for kt in range(K):
+                                if k!=kt:
+                                    #If the fold isnt the validation one
+                                    #We train the network
+                                    target_list,class_list,image_list=split_in_batches(target_fold[kt],class_fold[kt], image_fold[kt],b)
+                                    #nb of epochs
+                                    e=0
+                                    #If loss doesn't move anymore, we stop training
+                                    old_loss=10000
+                                    convergence=False
+                                    
+                                    while(e<max_epochs and convergence==False):
+                                        e+=1
+                                        for i in range(len(class_list)):
+                                            batch=image_list[i]
+                                            classes=class_list[i]
+                                            outs=torch.zeros(b,10)
+                                            for z in range(classes.shape[0]):
+                                                outs[z,:]=n(batch[z,0,:,:])
+                                            o.zero_grad() 
+                    
+                                            loss = criterion(outs,classes)
+                                            loss.backward()
+                                            o.step()
+                                            
+                                            if abs(loss.item())<0.00001:
+                                                #print("loss is very small")
+                                                convergence=True
+                                                break
+                                            if abs(loss.item()-old_loss)<0.0001:
+                                                #print("loss does not move")
+                                                convergence=True
+                                                break
+                                            old_loss=loss.item()
+                                        if convergence:
+                                            #print("convergence condition reached after",e,"epochs")
+                                            break
+                            if(convergence==False):
+                                #print("training done, no convergence")
                                 pass
-                            else:
-                                errors+=1
-                        print("errors",errors)
-                        perc_err=errors/(1000-t)
-                        acc=(1-perc_err)*100
-                        print("accuracy",acc)
+                            #We will now validate the network on fold k
+                            errors=0
+                            test_set=image_fold[k]
+                            test_class=class_fold[k]
+                            for idx in range(test_class.shape[0]):
+                                output=n(test_set[idx,0,:,:])
+                                predicted_class=output[0,:].max(0)[1]
+                                if predicted_class==test_class[idx].type(torch.LongTensor):
+                                    pass
+                                else:
+                                    errors+=1
+                            print("errors",errors)
+                            perc_err=errors/(1000//K)
+                            acc=(1-perc_err)*100
+                            print("accuracy",acc)
 def train_in_full(net,data,classes,activation,loss):
     pass
     
